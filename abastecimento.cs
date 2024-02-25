baa372570ed29ade70d411c811d3d98c49cc605f
thiambrosio12 <148159921+thiambrosio12@users.noreply.github.com>
2024-02-25 17:19:27 -0300

    terceiro commit
    apagando redundâncias

--- abastecimento.cs
+++ abastecimento.cs
@@ -10,17 +10,7 @@ class Program
         double consumoEtanol = 0.0;
         double consumoGasolina = 0.0;
 
-        while (true)
-        {
-            Console.WriteLine("Escolha uma opção:");
-            Console.WriteLine("1 - Calcular combustível");
-            Console.WriteLine("2 - Editar dados");
-            Console.WriteLine("3 - Sair do programa");
 
-            int opcao = Convert.ToInt32(Console.ReadLine());
-        }
-    }
-}
         while (true)
         {
             Console.WriteLine("Escolha uma opção:");
